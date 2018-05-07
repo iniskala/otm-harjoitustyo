ff681ef1caf9fb27b7b6ac8416e84ee61496938b
iniskala <iiro.niskala@helsinki.fi>
2018-05-07 16:42:59 +0300

    generoimisen viimeistely koodi ei toimi ollenkaan syy ei tiedossa. Ohjelma sanoo vikaa main metodissa mutta siihen ei ole koskettu.

--- harjoitustyo/seatinggenerator/src/main/java/seatinggenerator/generatoretc/Generator.java
+++ harjoitustyo/seatinggenerator/src/main/java/seatinggenerator/generatoretc/Generator.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import seatinggenerator.database.Database;
 import seatinggenerator.database.Participant;
 import seatinggenerator.database.ParticipantDao;
@@ -22,14 +23,22 @@ import seatinggenerator.database.ParticipantDao;
 public class Generator {
     private ParticipantDao help;
     private List<Participant> list1;
+    private List<Participant> alone;
     private List<Participant> withavec;
+    private List<Participant> withwish;
+    private List<Participant> withall;
+    private Listgenerator listgenerator;
 
     
     public Generator() throws ClassNotFoundException, SQLException {
         Database database = new Database("jdbc:sqlite:sitsit.db");
         this.help = new ParticipantDao(database);
+        this.listgenerator = new Listgenerator(this.list1);
         this.list1 = new ArrayList<Participant>(help.findAll());
-        this.withavec = new ArrayList<Participant>();
+        this.withavec = new ArrayList<Participant>(listgenerator.withavecList());
+        this.withall = new ArrayList<Participant>(listgenerator.withallList());
+        this.withwish = new ArrayList<Participant>(listgenerator.withwishList());
+        this.alone = new ArrayList<Participant>(listgenerator.aloneList());
     }
     
     /**
@@ -40,115 +49,61 @@ public class Generator {
                         
         FileWriter writer = new FileWriter("Seat_map.csv");
         
-        
+        Random rnd = new Random();
         int size = list1.size();
         int half = size / 2;
         int point = 0;
+        int alonei = 0;
+        int withaveci = 0;
+        int withalli = 0;
+        int withwishi = 0;
         for (int i = 0; i < list1.size(); i++) {
-            
+            int which = rnd.nextInt(4); 
             if (half == point) {
                 writer.append("\n");
             }
             
-            point++;
-            
-            if (!withavec.isEmpty() && half != (point)) {
-                for (int i3 = 0; i3 < withavec.size(); i3++) {
-                    writer.append(withavec.get(i).getName());                    
-                }
-                withavec.clear();
+             
+            if (which == 0 && alonei < this.alone.size()) {
+                writer.append(alone.get(alonei).getName());
+                alonei++;
+                point++;
+            } else {
+                which = which + 1;
             }
-            
-            if (!list1.get(i).getAvec().equals("Empty") && !list1.get(i).getWish().equals("Empty")) {
-                for (int i4 = 0; i4 < list1.size(); i4++) {
-                    if (list1.get(i4).getName().equals(list1.get(i).getAvec())) {
-                        if (half == (point)) {
-                            withavec.add(list1.get(i));
-                            withavec.add(list1.get(i4));
-                            list1.remove(i);                        
-                        if (i < i4) {
-                            list1.remove(i4 - 1);
-                        } else {
-                            list1.remove(i4);
-                        }
-                        i = 0;
-                        break;
-                        } else {
-                            writer.append(list1.get(i).getName() + ";");
-                            writer.append(list1.get(i4).getName() + ";");
-                            list1.remove(i);                        
-                            if (i < i4) {
-                                list1.remove(i4 - 1);
-                            } else {
-                                list1.remove(i4);
-                            }
-                            i = 0;
-                            break;
-                        }
-                    }
-                            
-                }
+            if (which == 1 && withaveci < this.withavec.size() && half != point + 2 && half != point + 1) {
+                writer.append(withavec.get(withaveci).getName());
+                withaveci++;
+                point++;
+                writer.append(withavec.get(withaveci).getName());
+                withaveci++;
+                point++;
+            } else {
+                which = which + 1;
             }
-            if (list1.get(i).getAvec().equals("Empty")) {
-                if (list1.get(i).getWish().equals("Empty")) {
-                    writer.append(list1.get(i).getName() + ";");
-                    list1.remove(i);
-                    i = 0;
-                } else {
-                    for (int i2 = 0; i2 < list1.size(); i2++) {
-                        if (list1.get(i2).getName().equals(list1.get(i).getWish())) {                            
-                            writer.append(list1.get(i).getName() + ";");
-                            if (half == (point)) {
-                                writer.append("\n");
-                            }
-                            writer.append(list1.get(i2).getName() + ";");
-                            list1.remove(i);
-                            if (i < i2) {
-                                list1.remove(i2 - 1);
-                            } else {
-                                list1.remove(i2);
-                            }
-                            i = 0;
-                            break;
-                        }
-                            
-                    }
-                    writer.append(list1.get(i).getName() + ";");
-                    list1.remove(i);
-                    i = 0;
-                }                     
-            } else if (list1.get(i).getWish().equals("Empty")) {
-                for (int i2 = 0; i2 < list1.size(); i2++) {
-                    if (list1.get(i2).getName().equals(list1.get(i).getAvec())) {
-                        if (half == (point)) {
-                            withavec.add(list1.get(i));
-                            withavec.add(list1.get(i2));
-                            list1.remove(i);                        
-                            if (i < i2) {
-                                list1.remove(i2 - 1);
-                            } else {
-                                list1.remove(i2);
-                            }
-                            i = 0;
-                            break;
-                        } else {
-                            writer.append(list1.get(i).getName() + ";");
-                            writer.append(list1.get(i2).getName() + ";");
-                            list1.remove(i);                        
-                            if (i < i2) {
-                                list1.remove(i2 - 1);
-                            } else {
-                                list1.remove(i2);
-                            }
-                            i = 0;
-                            break;
-                        }
-                    }
-                            
-                }
+            if (which == 2 && withwishi < this.withwish.size() && half != point + 2 && half != point + 1) {
+                writer.append(withwish.get(withwishi).getName());
+                withwishi++;
+                point++;
+                writer.append(withwish.get(withwishi).getName());
+                withwishi++;
+                point++;
+            } else {
+                which = which + 1;
             }
-            
-            
+            if (which == 3 && withalli < this.withall.size() && half != point + 2 && half != point + 1 && half != point +3) {
+                writer.append(withall.get(withalli).getName());
+                withalli++;
+                point++;
+                writer.append(withall.get(withalli).getName());
+                withalli++;
+                point++;
+                writer.append(withall.get(withalli).getName());
+                withalli++;
+                point++;
+            } else {
+                which = which - 1;
+            }
         }
         writer.flush();
         writer.close();
--- harjoitustyo/seatinggenerator/src/main/java/seatinggenerator/generatoretc/Listgenerator.java
+++ harjoitustyo/seatinggenerator/src/main/java/seatinggenerator/generatoretc/Listgenerator.java
@@ -92,6 +92,8 @@ public class Listgenerator {
             for (int i2 = 0; i < all.size(); i2++){
                 for (int i3 = 0; i3 < all.size(); i3++){
                     if (all.get(i).getAvec().equals(all.get(i2).getName()) && all.get(i).getWish().equals(all.get(i3).getName())) {
+                        
+                        
                         this.withall.add(all.get(i3));
                         this.withall.add(all.get(i));
                         this.withall.add(all.get(i2));
@@ -99,6 +101,19 @@ public class Listgenerator {
                         this.all.remove(i);
                         this.all.remove(i2);
                         this.all.remove(i3);
+                    } else if (all.get(i).getAvec().equals(all.get(i2).getName())) {
+                        this.withavec.add(all.get(i));
+                        this.withavec.add(all.get(i2));
+                        this.all.remove(i);
+                        this.all.remove(i2);
+                    } else if (all.get(i).getWish().equals(all.get(i3).getName())) {
+                        this.withwish.add(all.get(i));
+                        this.withwish.add(all.get(i3));
+                        this.all.remove(i);
+                        this.all.remove(i3);
+                    } else {
+                        this.alone.add(all.get(i));
+                        this.all.remove(i);
                     }
                 }
             }
